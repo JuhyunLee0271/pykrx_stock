5a20d2009ab42f164a588f22fe024784599191f0
JuhyunLee <wngus392@naver.com>
2021-11-23 00:55:06 +0900

    order by transaction_amount

--- RealtimeInfo.py
+++ RealtimeInfo.py
@@ -4,24 +4,25 @@ from datetime import datetime
 
 """
 RealtimeInfo 
-    KOSPI, KOSDAQ의 총 2467개의 종목들에 대해 매 10분마다 실시간(종가) 가격을 price_now 딕셔너리에 저장
+    KOSPI, KOSDAQ의 총 2467개의 종목들에 대해 매 10분마다 실시간(종가) 가격, 등락률, 거래량, 거래대금을 거래대금으로 정렬해 price_now 딕셔너리에 저장
 """
 
 # Class For "STOCK"
 class TodayInfoCrawler:
     def __init__(self):
         self.CODE = stock.get_market_ticker_list(market="KOSPI") + stock.get_market_ticker_list(market="KOSDAQ")
-        # self.today = str(datetime.now().date()).replace('-','')
-        self.today = "20211122"
+        self.today = str(datetime.now().date()).replace('-','')
         self.KOSPI = None
         self.KOSDAQ = None
         self.PRICE_NOW = None
     
+    # 거래대금 별로 정렬
     def UpdateStockPrice(self):
         self.KOSPI = stock.get_market_ohlcv_by_ticker(self.today, market="KOSPI")[['등락률', '종가', '거래량', '거래대금']].to_dict('index')
         time.sleep(1)
         self.KOSDAQ = stock.get_market_ohlcv_by_ticker(self.today, market="KOSDAQ")[['등락률', '종가', '거래량', '거래대금']].to_dict('index')
-        self.PRICE_NOW = dict(self.KOSPI, **self.KOSDAQ)
+        self.PRICE_NOW = dict(sorted(dict(self.KOSPI, **self.KOSDAQ).items(), key=lambda x: -x[1]['거래대금']))
+
 
 # def sched(sc):
 #     sc.UpdateStockPrice()
@@ -29,9 +30,9 @@ class TodayInfoCrawler:
 def main():
     # 종목 코드 정보: sc.CODE
     sc = TodayInfoCrawler(); sc.UpdateStockPrice()
-    print(sc.PRICE_NOW)
     # sc.PRICE_NOW
-    # {'095570': 5460, '006840': 24600, '027410': 6060, ...}
+    # 종목별 등락률, 종가, 거래량, 거래대금이 딕셔너리로 
+    
 
     # time_quantum = 10
     # schedule.every(time_quantum).minutes.do(sched, sc)
